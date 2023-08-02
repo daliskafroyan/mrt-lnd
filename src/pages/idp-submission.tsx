ecc1403c61f2883b6d19fc4d9d7dbd9b891fce4f
Daliska F. Royan <firsta@ritase.com>
2023-08-02 14:14:15 +0700

    feat: enhance form pengajuan idp

--- src/pages/idp-submission.tsx
+++ src/pages/idp-submission.tsx
@@ -10,99 +10,136 @@ import {
   Stack,
   Textarea,
   TextInput,
+  Button,
+  Badge,
 } from '@mantine/core';
 import { DatePickerInput } from '@mantine/dates';
+import { IconPencil } from '@tabler/icons-react';
 import { useState } from 'react';
 
 export default function IDPSubmission() {
   const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
+  const [isDiajukan, setIsDiajukan] = useState<boolean>(false);
   return (
     <DashboardLayout>
-      <Title order={4}>Form Pengajuan IDP</Title>
-      <Group spacing="xl" mt={40}>
-        <Stack style={{ flex: 7 }} spacing="xl">
-          <Stack>
-            <Stack spacing={0}>
-              <Text>Jenis Kompetensi Program</Text>
-              <Text c="dimmed">Informasi Program yang akan diajukan</Text>
-            </Stack>
-            <Group spacing="lg" grow>
-              <Stack>
-                <Select
-                  label="Jenis Kompetensi"
-                  placeholder="Technical Skill Competency"
-                  data={[
-                    { value: 'react', label: 'React' },
-                    { value: 'ng', label: 'Angular' },
-                  ]}
-                />
-                <Select
-                  label="Kategori Program"
-                  placeholder="Acceleration Program"
-                  data={[
-                    { value: 'react', label: 'React' },
-                    { value: 'ng', label: 'Angular' },
-                  ]}
-                />
-                <Select
-                  label="Jenis Program"
-                  placeholder="Pelatihan"
-                  data={[
-                    { value: 'react', label: 'React' },
-                    { value: 'ng', label: 'Angular' },
-                  ]}
-                />
+      <Box p="xl">
+        <Group position="apart">
+          <Group>
+            <Title order={4}>Form Pengajuan IDP</Title>
+            {isDiajukan ? (
+              <Badge size="lg" radius="xs" variant="light">
+                diajukan
+              </Badge>
+            ) : null}
+          </Group>
+          {isDiajukan ? null : (
+            <Button
+              variant="default"
+              leftIcon={<IconPencil />}
+              onClick={() => setIsDiajukan(!isDiajukan)}
+            >
+              Edit
+            </Button>
+          )}
+        </Group>
+        <Group spacing="xl" mt={40}>
+          <Stack style={{ flex: 7 }} spacing="xl">
+            <Stack>
+              <Stack spacing={0}>
+                <Text>Tentang Program</Text>
+                <Text c="dimmed">Informasi Program yang akan diajukan</Text>
               </Stack>
-              <Textarea
-                placeholder="contoh: Pelatihan ISO 3100"
-                label="Judul Program"
-                minRows={7}
-                mb="auto"
-              />
-            </Group>
-          </Stack>
-          <Stack>
-            <Stack spacing={0}>
-              <Text>Jenis Kompetensi Program</Text>
-              <Text c="dimmed">Informasi Program yang akan diajukan</Text>
-            </Stack>
-            <Group spacing="lg" grow>
-              <Stack>
-                <TextInput label="Penyelenggara" />
-                <DatePickerInput
-                  type="range"
-                  label="Pick dates range"
-                  placeholder="Pick dates range"
-                  value={value}
-                  onChange={setValue}
-                  maw={400}
+              <Group spacing="lg" grow>
+                <Stack>
+                  <Select
+                    label="Jenis Kompetensi"
+                    placeholder="Technical Skill Competency"
+                    data={[
+                      { value: 'react', label: 'React' },
+                      { value: 'ng', label: 'Angular' },
+                    ]}
+                  />
+                  <Select
+                    label="Kategori Program"
+                    placeholder="Acceleration Program"
+                    data={[
+                      { value: 'react', label: 'React' },
+                      { value: 'ng', label: 'Angular' },
+                    ]}
+                  />
+                  <Select
+                    label="Jenis Program"
+                    placeholder="Pelatihan"
+                    data={[
+                      { value: 'react', label: 'React' },
+                      { value: 'ng', label: 'Angular' },
+                    ]}
+                  />
+                </Stack>
+                <Textarea
+                  placeholder="contoh: Pelatihan ISO 3100"
+                  label="Judul Program"
+                  minRows={7}
+                  mb="auto"
                 />
-                <CurrencyInput label="Estimasi Biaya" />
-              </Stack>
-              <Textarea
-                placeholder="contoh: Pelatihan ISO 3100"
-                label="Judul Program"
-                minRows={7}
-                mb="auto"
-              />
-            </Group>
-          </Stack>
-          <Stack>
-            <Stack spacing={0}>
-              <Text>Dokumen Pendukung</Text>
-              <Text c="dimmed">Unggah bukti terkait</Text>
+              </Group>
             </Stack>
-            <DropzoneImage />
-            <Text size="sm" c="dimmed">
-              *Anda dapat mengupload brosur atau flyer yang akan diajukan
-            </Text>
+            <Stack>
+              <Stack spacing={0}>
+                <Text>Jenis Kompetensi Program</Text>
+                <Text c="dimmed">Informasi Program yang akan diajukan</Text>
+              </Stack>
+              <Group spacing="lg" grow>
+                <Stack>
+                  <TextInput label="Penyelenggara" />
+                  <DatePickerInput
+                    type="range"
+                    label="Pick dates range"
+                    placeholder="Pick dates range"
+                    value={value}
+                    onChange={setValue}
+                    maw={400}
+                  />
+                  <CurrencyInput label="Estimasi Biaya" />
+                </Stack>
+                <Textarea
+                  placeholder="contoh: Pelatihan ISO 3100"
+                  label="Judul Program"
+                  minRows={7}
+                  mb="auto"
+                />
+              </Group>
+            </Stack>
+            <Stack>
+              <Stack spacing={0}>
+                <Text>Dokumen Pendukung</Text>
+                <Text c="dimmed">Unggah bukti terkait</Text>
+              </Stack>
+              <DropzoneImage />
+              <Text size="sm" c="dimmed">
+                *Anda dapat mengupload brosur atau flyer yang akan diajukan
+              </Text>
+            </Stack>
           </Stack>
-        </Stack>
-        <Box mb="auto" style={{ flex: 3 }}>
-          <Title order={3}>Catatan IDP</Title>
-          <Text c="dimmed">Catatan approval/reject dari atasan</Text>
-        </Box>
-      </Group>
+          <Stack style={{ flex: 3 }} justify="space-between" mb="auto">
+            <Stack spacing={0}>
+              <Title order={3}>Catatan IDP</Title>
+              <Text c="dimmed">Catatan approval/reject dari atasan</Text>
+            </Stack>
+          </Stack>
+        </Group>
+        <Group position="right" mt="xl">
+          <Button variant="default" disabled={!isDiajukan}>
+            Batal
+          </Button>
+          <Button
+            onClick={() => setIsDiajukan(!isDiajukan)}
+            disabled={!isDiajukan}
+          >
+            Simpan
+          </Button>
+        </Group>
+      </Box>
     </DashboardLayout>
   );
 }
